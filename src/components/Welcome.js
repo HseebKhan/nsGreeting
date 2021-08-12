5ca82bc8d478ae63aa9ed25bac0a8e9143d33256
unknown <abdul.haseeb2097@gmail.com>
2021-08-12 20:21:49 +0530

    minor changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,12 @@
 // write code for Welcome component here
-import React from 'react'
+import React from "react";
 
 function Welcome(props) {
-    return (
-        <div>
-            Hey {props.name}!
-            <h2>Welcome to Newton School.</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Hey {props.name}! Welcome to Newton School.</h2>
+    </div>
+  );
 }
 
-export default Welcome
+export default Welcome;
