9a45df86da9510dcd83f6cbff74210653cc6258e
unknown <abdul.haseeb2097@gmail.com>
2021-08-12 20:16:10 +0530

    initial commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+// write code for Welcome component here
+import React from 'react'
+
+function Welcome(props) {
+    return (
+        <div>
+            Hey {props.name}!
+            <h2>Welcome to Newton School.</h2>
+        </div>
+    )
+}
+
+export default Welcome
